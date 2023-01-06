ae55763e5b11c1a13cf63a108c20bbca16fb900e
Felipe Fernandez <FelipeFelizx@gmail.com>
2023-01-06 01:45:16 -0300

    Feature: añadir Footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import  Todo  from './Components/Todo';
+import { Footer } from './Components/Footer';
 
 
 function App() {
   return (
     <div >
       <Todo/>
+      <Footer/>
     </div>
   );
 }
